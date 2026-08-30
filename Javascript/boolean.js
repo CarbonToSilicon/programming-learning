a75e87bb0e9059deea852b935518fb25fc6f9c25
Tusharex13 <tusharbagora6466@gmail.com>
2026-08-30 19:47:31 +0530

    js boolean

---
+++ Javascript/boolean.js
@@ -0,0 +1,14 @@
+console.log(typeof false);
+
+console.log(3 > 5);
+console.log(3 > 5 - 3);
+
+console.log(7 < 7);
+console.log(7 <= 7);
+
+console.log(5 == '5.0')
+console.log(5 === '5.0')
+
+console.log(5 != '5.')
+console.log('5.1' != 5) // it first converts to number than compares but is not recommended
+console.log(5 !== '5.0')
