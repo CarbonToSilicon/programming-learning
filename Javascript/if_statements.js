3fb557d02c60bfa8d3255a52e917131a5e2d2998
Tusharex13 <tusharbagora6466@gmail.com>
2026-08-30 21:25:08 +0530

    writing a game

---
+++ Javascript/if_statements.js
@@ -0,0 +1,35 @@
+
+const age = 120;
+
+/* if (age >= 18) {
+  console.log("You can drive");
+} */
+
+
+/* if (age >= 18) {
+  console.log('You can drive');
+} else {
+  console.log('You cannot drive');
+} */
+
+
+/* if (age >= 18) {
+  console.log('You can drive');
+} else if (age >= 16) {
+  console.log('You are almost aligible');
+} else {
+  console.log('You cannot drive')
+} */
+
+
+if (age >= 120) {
+  console.log('You are a god i bow you');  
+} else if (age >= 18) {
+  if (age >= 80) {
+    console.log('It is too risky to drive');
+  } else {
+    console.log('You can drive');
+  }
+} else {
+  console.log('You are not aligible to drive');
+}
