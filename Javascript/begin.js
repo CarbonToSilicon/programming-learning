157546e0f97362a87e3f134145ae7b7393cefbd4
Tusharex13 <tusharbagora6466@gmail.com>
2026-08-30 19:21:00 +0530

    learning Javascript

---
+++ Javascript/begin.js
@@ -0,0 +1,14 @@
+// --- Your Python-style code goes below ---
+
+let name = "Alex";
+let score = 95;
+
+console.log("Hello", name);
+console.log(`Your score is ${score}`);
+
+score = 19;
+console.log(`Score = ${score}`);
+
+
+
+
